4d9aa0b534a7339687c74af07e8acac0cdc374f4
Bikram48 <chandbikram001@gmail.com>
2021-12-31 15:41:31 +0545

    add - meta description by actions

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
     function post_excerpt_length( $excerpt ) {
-        
+
         if( has_excerpt() ){
             $length = wp_trim_words( get_the_excerpt(), apply_filters( "excerpt_length",30 ));
         }
@@ -9,3 +9,9 @@
     }
 
     add_filter( 'the_excerpt', 'post_excerpt_length' );
+
+    function meta_description() {
+        echo '<meta name="description" content="This is the description">';
+    }
+    
+    add_action( 'wp_head', 'meta_description' );
