a971bec736af3b2b2227b8636a5d128a8e15976c
Bikram48 <chandbikram001@gmail.com>
2021-12-31 14:38:04 +0545

    retrieve - all posts and post contents

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <h1>Custom Theme</h1>
+<?php
+ if ( have_posts() ):
+        while ( have_posts() ) : the_post();?>
+        <article class="post">
+            <h2><a href="<?php the_permalink() ?>"><?php echo the_title(); ?></a></h2>
+            <?php echo the_content();?>
+            <?php echo the_excerpt(); ?>
+        </article>
+        <?php
+        endwhile;
+    else :
+        echo '<p>There are no posts!</p>';
+    endif;
